6a2512cc02b2aa78fd619fb86644b9fcce0ff5c4
Trist4n <tristan@defachel.fr>
2026-08-04 09:49:34 +0200

    fix(hyprland config): Dolphin default openning app

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -29,7 +29,7 @@ hl.env("HYPRCURSOR_SIZE", "24")
 hl.env("LIBVA_DRIVER_NAME", "nvidia")
 hl.env("__GLX_VENDOR_LIBRARY_NAME", "nvidia")
 hl.env("QT_QPA_PLATFORMTHEME", "qt6ct")
-
+hl.env("XDG_MENU_PREFIX", "arch-")
 
 -----------------------
 ----- PERMISSIONS -----
