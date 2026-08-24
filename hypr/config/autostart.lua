b27dce1318088d7a24aaa563f9e0f61b63e36524
Trist4n <tristan@defachel.fr>
2026-08-24 10:20:15 +0200

    fix(hypr): C Cedilla in Electron App

--- hypr/config/autostart.lua
+++ hypr/config/autostart.lua
@@ -3,11 +3,11 @@
 -------------------
 
 -- See https://wiki.hypr.land/Configuring/Basics/Autostart/
-local globalVariables = require('config/global-variables')
 
 hl.on("hyprland.start", function()
     hl.exec_cmd("nm-applet")
     hl.exec_cmd("hyprpm reload")
+    hl.exec_cmd("fcitx5 -d")
     hl.exec_cmd("awww-daemon & hypridle")
     hl.exec_cmd("zen-browser", { workspace = "1" })
     hl.exec_cmd("vscodium", { workspace = "2" })
